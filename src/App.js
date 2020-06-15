b154938a7303cd1bb977a6a2d2dfd3e5f9fa8df8
gouravsinghal <49369629+gouravsinghal@users.noreply.github.com>
2020-06-15 22:08:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Contacts from "./components/Contacts";
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Contacts.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Contacts extends Component {
+  render() {
+    return (
+      <div>
+        <h4> gourav</h4>
+        <ul>
+          <li> Email : dhids</li>
+          <li>Phone: ddcbkbd</li>
+        </ul>
+      </div>
+    );
+  }
+}
+export default Contacts;
